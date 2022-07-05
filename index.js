43e30021e57a4c6c625f13802822465c5da87ffd
iamtimbun <timothytimbun@gmail.com>
2022-07-05 20:12:55 +0100

    Javascript Calculator

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ let display = document.getElementById('display');
 
 let Buttons = Array.from(document.getElementsByClassName("Button"));
 
+let evaluated = false;
+
 Buttons.map( Button => {
     Button.addEventListener("click", (e) => {
        switch(e.target.innerText){
@@ -16,11 +18,16 @@ Buttons.map( Button => {
             case '=':
                 try {
                         display.innerText = eval(display.innerText);
+                        evaluated = true;
                     }catch {
                         display.innerText = "Error!";
                     }
                     break;
                 default:
+                    if (evaluated == true) {
+                        display.innerText = "";
+                        evaluated = false;
+                    }
                     display.innerText += e.target.innerText;
        }
     });
